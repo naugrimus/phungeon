513035628648b984dcb0aeb88c1dddeb0b62c5ab
Niels Veenstra <nielsveenstra@lfh.nl>
2026-01-09 13:25:12 +0100

    type:feature
    added collision detection

--- engine/States/DungeoneeringState.php
+++ engine/States/DungeoneeringState.php
@@ -2,47 +2,79 @@
 
 namespace Engine\States;
 
-use App\Helpers\RoomGenerator;
 use Engine\Core\GameData;
 use Engine\Handlers\InputHandler;
 use Engine\Interfaces\StateInterface;
-use Engine\Models\Room;
 
-class DungeoneeringState  extends AbstractState implements StateInterface
+class DungeoneeringState extends AbstractState implements StateInterface
 {
-
     const NAME = 'Dungeoneering';
+
+    protected ?GameData $gameData;
+
+    protected ?InputHandler $inputHandler;
 
     public function handle(GameData $gameData, InputHandler $inputHandler)
     {
+        $this->gameData = $gameData;
+        $this->inputHandler = $inputHandler;
+        $this->detectMovemnet();
 
+    }
+
+    protected function detectMovemnet(): void
+    {
         // read the input
-        switch($inputHandler::read()) {
+        $y = $this->gameData->getPlayer()->getPosition()->getY();
+        $x = $this->gameData->getPlayer()->getPosition()->getX();
+
+        switch ($this->inputHandler::read()) {
             case 'w':
-                $y = $gameData->getPlayer()->getPosition()->getY();
                 $y--;
-                $gameData->getPlayer()->getPosition()->setY($y);
-                $gameData->updateTurns();
+                if (! $this->detectBlocking($x, $y)) {
+                    $this->gameData->getPlayer()->getPosition()->setY($y);
+                    $this->gameData->updateTurns();
+                }
+
                 break;
             case 's':
-                $y = $gameData->getPlayer()->getPosition()->getY();
                 $y++;
-                $gameData->getPlayer()->getPosition()->setY($y);
-                $gameData->updateTurns();
+                if (! $this->detectBlocking($x, $y+1)) {
+                    $this->gameData->getPlayer()->getPosition()->setY($y);
+                    $this->gameData->updateTurns();
+                }
                 break;
             case 'a':
-                $x = $gameData->getPlayer()->getPosition()->getX();
                 $x--;
-                $gameData->getPlayer()->getPosition()->setX($x);
-                $gameData->updateTurns();
+                if (! $this->detectBlocking($x, $y)) {
+                    $this->gameData->getPlayer()->getPosition()->setX($x);
+                    $this->gameData->updateTurns();
+                }
                 break;
             case 'd':
-                $x = $gameData->getPlayer()->getPosition()->getX();
                 $x++;
-                $gameData->getPlayer()->getPosition()->setX($x);
-                $gameData->updateTurns();
+                if (! $this->detectBlocking($x, $y)) {
+                    $this->gameData->getPlayer()->getPosition()->setX($x);
+                    $this->gameData->updateTurns();
+                }
                 break;
         }
+    }
 
+    protected function detectBlocking($playerX, $playerY): bool
+    {
+        $rooms = $this->gameData->getRooms();
+        $room = $rooms[$this->gameData->getCurrentRoom()];
+        $map = $room->getMap();
+        foreach ($map as $col => $value) {
+            $array = str_split($value);
+            foreach ($array as $row => $char) {
+                if ($row == $playerX && $char == '#' && $col == $playerY) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
